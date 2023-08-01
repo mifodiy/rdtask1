f4da8069dfc55cafb32b23452ac5b1ba994c94b1
mifodiy911 <mifodiy911@gmail.com>
2023-08-01 16:14:31 +0300

    Create script makeNoteItem

---
+++ js/data/makeNoteItem.js
@@ -0,0 +1,18 @@
+export const makeNoteItem = (note) => {
+  return `<li class="note__item ${note.id}">
+  <div class="note__name">
+    <img class="note__img" src="${note.img}" alt="${note.category}">
+    <span>${note.name}</span>
+  </div>
+  <div class="note__created">${note.creationDate}</div>
+  <div class="note__category">${note.category}</div>
+  <div class="note__content">${note.content}</div>
+  <div class="note__dates">${note.dates}</div>
+  <div class="note__icons">
+    <img class="note__edit" src="image/edit.svg" alt="edit">
+    <img class="note__archive" src="image/archive.svg" alt="archive">
+    <img class="note__delete" src="image/trash.svg" alt="delete">
+  </div>
+</li>`
+}
+
